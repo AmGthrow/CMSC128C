128a01d164c0513d881f3e77c86c15f40d978e55
Carryl21 <cjdelatorre1@up.edu.ph>
2022-03-08 20:35:41 +0800

    added the home route

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,14 @@
 import './App.css';
+import { Routes, Route} from "react-router-dom";
+import Home from './webpages/Home';
 
 function App() {
   return (
     <div className="App">
-      
-
+      <h1>Welcome to React Router!</h1>
+      <Routes>
+        <Route path="/" element={<Home/>} />
+      </Routes>
     </div>
   );
 }
