ea4a308e93530c7259248e7fb4962e9f078a8d25
Jethro Modequillo <jethro.modequillo@mensaphilippines.org>
2022-03-08 10:38:01 +0800

    FEAT: Added temp structure for Email waitlist

--- server/server.js
+++ server/server.js
@@ -1,1 +1,13 @@
-console.log('hello')
+const express = require('express');
+const app = express();
+
+app.listen(9000, () =>
+    console.log("Listening on port 9000")
+)
+
+// TODO: Move this to POST request
+app.get("/waitlist", (req, res) => {
+    res.send("Thanks for signing up to our waitlist!");
+    console.log("Client added to waitlist");
+})
+
