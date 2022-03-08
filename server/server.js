6e740eff2a4e02d8024e0fc4069f9e93aefd616a
Jethro Modequillo <jethro.modequillo@mensaphilippines.org>
2022-03-08 10:59:51 +0800

    FEAT: Added sample for POST method from frontend to backend

--- server/server.js
+++ server/server.js
@@ -5,13 +5,22 @@ app.listen(9000, () =>
     console.log("Listening on port 9000")
 )
 
+app.use(
+    express.urlencoded({
+        extended: true
+    })
+)
+
+app.use(express.json())
+
 app.get('/', (req, res) => {
     res.sendFile('src/index.html', { root: __dirname })
     console.log("User connected")
 })
 
 app.post("/waitlist", (req, res) => {
+    const email = req.body.email;
     res.send("Thanks for signing up to our waitlist!");
-    console.log("Client added to waitlist");
+    console.log(`Client ${email} added to waitlist`);
 })
 
