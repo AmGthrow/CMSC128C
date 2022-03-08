c485f28fea2c6c409bce7d877a87057ceadfaeae
Jethro Modequillo <jethro.modequillo@mensaphilippines.org>
2022-03-08 10:49:04 +0800

    TEST: Added sample index file for adding to waitlist

--- server/server.js
+++ server/server.js
@@ -5,8 +5,12 @@ app.listen(9000, () =>
     console.log("Listening on port 9000")
 )
 
-// TODO: Move this to POST request
-app.get("/waitlist", (req, res) => {
+app.get('/', (req, res) => {
+    res.sendFile('src/index.html', { root: __dirname })
+    console.log("User connected")
+})
+
+app.post("/waitlist", (req, res) => {
     res.send("Thanks for signing up to our waitlist!");
     console.log("Client added to waitlist");
 })
