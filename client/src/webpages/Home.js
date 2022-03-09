f0aaafda6d587b4ffc8fdcff77c439054c386b14
Carryl21 <cjdelatorre1@up.edu.ph>
2022-03-09 22:09:04 +0800

    imported the footer componenent to homepage

--- client/src/webpages/Home.js
+++ client/src/webpages/Home.js
@@ -1,7 +1,11 @@
 import React from 'react'
+import Footer from '../components/Footer'
 
 export default function Home() {
   return (
-    <div>Home</div>
+    <div>
+      <h> Home</h>
+      <Footer/>
+    </div>
   )
 }
