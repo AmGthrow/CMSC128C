d03bb14913385baf0efe4a1a11e137fe41110296
Jethro Modequillo <jethro.modequillo@mensaphilippines.org>
2022-03-10 21:42:34 +0800

    Added Hero Shot component

--- client/src/webpages/Home.js
+++ client/src/webpages/Home.js
@@ -4,6 +4,21 @@ import hero_bg from "./hero-bg.jpg";
 
 export default function Home() {
   return (
-    <div>Home</div>
+    <div>
+      <HeroShot></HeroShot>
+    </div>
   )
 }
+
+function HeroShot() {
+  return (
+    <div id="hero-shot" style={{
+      backgroundImage: `url("${hero_bg}")`,
+      backgroundSize: "cover",
+      backgroundPosition: "right center"
+    }}>
+      <img src={logo_textless} id="hero__logo" alt="Codercademy logo"></img>
+      <h1>We are <b>Codercademy</b></h1>
+      <h3>cultivating learning since 2020</h3>
+    </div>
+  )
