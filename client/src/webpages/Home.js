307ab652283211137c75ac25504fd51d7670231f
Jethro Modequillo <jethro.modequillo@mensaphilippines.org>
2022-03-10 21:42:21 +0800

    Added static assets

--- client/src/webpages/Home.js
+++ client/src/webpages/Home.js
@@ -1,4 +1,6 @@
-import React from 'react'
+import React from 'react';
+import logo_textless from "./logo_textless.png";
+import hero_bg from "./hero-bg.jpg";
 
 export default function Home() {
   return (
