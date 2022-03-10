33a1f2452f221e5afcaf92eda7be82208df2b24f
Jethro Modequillo <jethro.modequillo@mensaphilippines.org>
2022-03-10 21:53:39 +0800

    Moved some styling from JS to CSS

--- client/src/webpages/Home.js
+++ client/src/webpages/Home.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import './landingPage.css';
 import logo_textless from "./logo_textless.png";
-import hero_bg from "./hero-bg.jpg";
 
 export default function Home() {
   return (
@@ -13,11 +12,7 @@ export default function Home() {
 
 function HeroShot() {
   return (
-    <div id="hero-shot" style={{
-      backgroundImage: `url("${hero_bg}")`,
-      backgroundSize: "cover",
-      backgroundPosition: "right center"
-    }}>
+    <div id="hero-shot">
       <img src={logo_textless} id="hero__logo" alt="Codercademy logo"></img>
       <h1>We are <b>Codercademy</b></h1>
       <h3>cultivating learning since 2020</h3>
