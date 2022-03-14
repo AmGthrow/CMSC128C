dd6a4caaf0d9e1821e06bd5d60fcb1d05bc27605
Carryl21 <cjdelatorre1@up.edu.ph>
2022-03-14 12:51:55 +0800

    added send email banner

---
+++ client/src/domain/SendEmailBanner/SendEmailBanner.js
@@ -0,0 +1,30 @@
+import React from 'react'
+import EmailInputField from './EmailInputField/EmailInputField'
+import './SendEmailBanner.css'
+
+function IndexFingerPoint () {
+    return (
+        <div className='index-finger-container'>
+            <img 
+            src={require('../../assets/index-finger-point.png')} 
+            alt='index finger point'/>
+        </div>
+    )
+}
+
+export default function EnrollNowBanner() {
+  return (
+    <div className='enroll-now-banner'>
+        <div className='content'>
+        {/* email-input-container */}
+            <div className='email-input-container'>
+                <p className='message-1'>Enroll Now!</p>
+                <p className='message-2'>Leave us your email, and we'll contact you!</p>
+                <EmailInputField />
+            </div>
+            {/* logo */}
+            <IndexFingerPoint />
+        </div>
+    </div>
+  )
+}
