de1e14ff81ebe2a1959bfa70b0193cafb0efdd88
Carryl21 <cjdelatorre1@up.edu.ph>
2022-03-14 13:46:34 +0800

    added hero shot banner

---
+++ client/src/domain/HeroShot/HeroShot.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import './HeroShot.css'
+import logo_textless from '../../assets/logo_textless.png'
+
+export default function HeroShot() {
+    return (
+      <div className='hero-shot-container'>
+          <div className="content">
+            <img src={logo_textless} id="hero__logo" alt="Codercademy logo"></img>
+            <h1>We are <b>Codercademy</b></h1>
+            <h3>cultivating learning since 2020</h3>
+          </div>
+      </div>
+    )
+  }
