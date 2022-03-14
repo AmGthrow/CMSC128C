56ca8f8677edb233a1a92e4cc7c179bcc5505919
Carryl21 <cjdelatorre1@up.edu.ph>
2022-03-14 17:13:55 +0800

    added form logic

--- client/src/domain/SendEmailBanner/EmailInputField/EmailInputField.js
+++ client/src/domain/SendEmailBanner/EmailInputField/EmailInputField.js
@@ -1,15 +1,38 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './EmailInputField.css'
+import {saveEmailAddress} from '../../../api/FetchLogic/saveEmailAddress'
 
 export default function EmailInputField() {
+  const [emailAddress, setEmailAddress] = useState ('')
+
+  const handleChange = e => {
+    setEmailAddress(e.target.value)
+  }
+
+  const handleSubmit = e => {
+    e.preventDefault()
+    const email = {'email': emailAddress}
+    handleSubmitEmail(email)
+    setEmailAddress('')
+  }
+
+  const handleSubmitEmail = async (email) =>  {
+    const {error} = await saveEmailAddress(email)
+    if(error) return console.log(error)
+  }
+
   return (
     <div className='input-field-container'>
-        <form>
-            <input placeholder='jdelacruz@gmail.com' />
-            <button>
-                <p className='button-text'>Send</p>
-            </button>
-        </form>
+      <form onSubmit={handleSubmit}>
+          <input 
+            value={emailAddress}
+            type='email'
+            onChange={handleChange}
+            placeholder='jdelacruz@gmail.com'/>
+          <button type='submit'>
+            <p className='button-text'>Send</p>
+          </button>
+      </form>
     </div>
   )
 }
