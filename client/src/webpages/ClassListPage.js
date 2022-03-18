28798d7c19a6bcc323af8b0ff1577db689ca5e26
Carryl21 <cjdelatorre1@up.edu.ph>
2022-03-19 00:40:36 +0800

    created class list page

---
+++ client/src/webpages/ClassListPage.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import ClassList from '../domain/ClassList/ClassList'
+
+export default function ClassListPage() {
+  return (
+    <>
+      <ClassList/>
+    </>
+  )
+}
