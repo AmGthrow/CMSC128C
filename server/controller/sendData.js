bb5a60b0416ecd7705b9351d1b294aa9704f2680
Jethro Modequillo <jethro.modequillo@mensaphilippines.org>
2022-03-24 09:23:38 +0800

    Move waitlist logic to \server\controller

--- server/controller/sendData.js
+++ server/controller/sendData.js
@@ -1,5 +1,18 @@
-// require from the model 
+// require from the model
+const { addToWaitlist } = require('../database/spreadsheet');
 
 exports.sendData = async function (req, res, next) {
-    res.send({'message': 'ok'})
+    res.send({ 'message': 'ok' })
+}
+
+exports.saveToWaitlist = (req, res) => {
+    const email = req.body.email;
+    if (!email) {
+        res.status(400).send("email parameter is required");
+        return;
+    }
+
+    addToWaitlist(email);
+    console.log(`Client ${email} added to waitlist`);
+    res.status(201).send(`Added ${email} to waitlist`)
 }
