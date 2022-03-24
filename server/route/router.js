232e88b96a12ef914b97f5e68806c0ed37d26f34
Jethro Modequillo <jethro.modequillo@mensaphilippines.org>
2022-03-24 09:23:48 +0800

    Add API endpoint for saving to waitlist

--- server/route/router.js
+++ server/route/router.js
@@ -1,8 +1,31 @@
-const router = require('express').Router() 
-const {sendData} =  require('../controller/sendData')
+const router = require('express').Router()
+const { sendData, saveToWaitlist } = require('../controller/sendData')
 
 
 router.get('/sample', sendData)
 
+/**
+ * @swagger
+ * /api/waitlist:
+ *   post:
+ *     description: Store a client's email into the waitlist
+ *     parameters:
+ *       - name: email
+ *         description: Client's email address
+ *         in: body
+ *         schema:
+ *           type: object
+ *           required:
+ *             - email
+ *           properties:
+ *             email:
+ *               type: string
+ *     responses: 
+ *       200:
+ *         description: Success
+ *       400:
+ *         description: Email parameter is missing
+ */
+router.post("/waitlist", saveToWaitlist)
 
 module.exports = router
