f8f8d6e21911e103deef56fc03572f944696d462
Jethro Modequillo <jethro.modequillo@mensaphilippines.org>
2022-03-24 08:46:44 +0800

    Add Swagger documentation in server setup

--- server/server.js
+++ server/server.js
@@ -2,6 +2,8 @@ require('dotenv').config({ path: '.env' })
 const cors = require('cors')
 const router = require('./route/router')
 const express = require('express')
+const swaggerUi = require('swagger-ui-express');
+const swaggerJsDoc = require('swagger-jsdoc');
 
 /* *set up app */
 const app = express()
@@ -18,6 +20,22 @@ app.use(
 )
 app.use(express.json())
 app.use('/api', router) // router: base
+
+// Config for API documentation
+const swaggerOptions = {
+    swaggerDefinition: {
+        info: {
+            title: 'Codercademy API',
+            'version': '1.0.0'
+        }
+    },
+    apis: ['server.js']
+};
+
+const swaggerDocs = swaggerJsDoc(swaggerOptions);
+
+// Serve documentation for API endpoints on /api-docs
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocs))
 
 /* listen to the port */
 app.listen(port, () => console.log(`Listening on port ${port}`))
