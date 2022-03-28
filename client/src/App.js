a59984ddcacf921a3b2fac0cb97d3c9ea4cb4654
Carryl21 <cjdelatorre1@up.edu.ph>
2022-03-28 21:43:02 +0800

    added the route for AccountingDashboard.js

--- client/src/App.js
+++ client/src/App.js
@@ -2,7 +2,7 @@ import './App.css';
 import { Routes, Route} from "react-router-dom";
 import LandingPage from './webpages/LandingPage';
 import ClassListPage from './webpages/ClassListPage';
-import Dashboard from './webpages/Dashboard';
+import AccountingDashboard from  './webpages/AccountingDashboard'
 
 function App() {
   return (
@@ -10,7 +10,7 @@ function App() {
       <Routes>
         <Route path="/" element={<LandingPage/>} />
         <Route path="/class-list" element={<ClassListPage/>} />
-        <Route path="/dashboard" element={<Dashboard/>} />
+        <Route path="/accounting/*" element={<AccountingDashboard/>} />
       </Routes>
     </div>
   );
