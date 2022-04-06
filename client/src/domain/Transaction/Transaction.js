4ea725a540b0c1ec75d993ea86d661c9ced673cb
Carryl21 <cjdelatorre1@up.edu.ph>
2022-04-07 06:14:22 +0800

    added the create and list component

--- client/src/domain/Transaction/Transaction.js
+++ client/src/domain/Transaction/Transaction.js
@@ -1,82 +1,12 @@
-import TransactionLogic from './TransactionLogic'
-import {Table,Thead,Tbody,Tr,Th, Td, Flex, Button} from '@chakra-ui/react'
-import { createContext, useContext } from 'react'
-
-const TransactionContext = createContext()
+import Create from './Create/Create'
+import List from './List/List'
+import {Flex} from '@chakra-ui/react'
 
 export default function Transaction() {
-
-    const value  = TransactionLogic()
-
-    return (
-        <TransactionContext.Provider value={value}>
-            <Flex w='80%' alignItems='center' justifyContent='center' p='6em' flexDir='column' > 
-                <TransactionTable />
-                <Pagination/>
-            </Flex>
-        </TransactionContext.Provider>
-    )
-}
-
-function TransactionTable () {
-
-    const { transactions } = useContext(TransactionContext)
-
-    return(
-        <Table size='sm' colorScheme='teal'>
-            <TableHead/>
-            {transactions.map(data => <TableBody key={data.id} data={data}/>)}
-        </Table>
-    )
-}
-
-
-function TableHead () {
-
-    const { header } = useContext(TransactionContext)
-
-    return(
-        <Thead>
-            <Tr>
-              { header.map((key, index) => { return <Th key={index}>{key.toUpperCase()}</Th> })}
-            </Tr>
-        </Thead>
-    )
-}
-
-function TableBody ({data}) {
-
-    return (
-        <Tbody>
-            <Tr >
-                {Object.values(data).map((key, index) => { return <Td key={index}> {key} </Td>})}
-            </Tr>
-        </Tbody>
-    )
-}
-
-function Pagination () {
-
-    const { pagination } = useContext(TransactionContext)
-
-    return( 
-        pagination.length  > 1 && (
-            <Flex>
-                {pagination.map((_, index) => <PaginationButton key={index} index={index} />)}
-            </Flex>
-        )
-    )
-}
-
-
-function PaginationButton ({index}){
-
-    const { fetchMoreTransactions } = useContext(TransactionContext)
-
-    return(
-        <Button
-            onClick={() => fetchMoreTransactions(index)}>
-            {index + 1}
-        </Button>
-    )
+  return (
+    <Flex w='80%' p='6em' flexDir='column'>
+        <Create />
+        <List />
+    </Flex>
+  )
 }
