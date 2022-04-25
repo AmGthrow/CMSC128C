b062a7140eedc5f08601c2c29a9ad38e57eee71f
Jethro Modequillo <jethro.modequillo@mensaphilippines.org>
2022-04-25 13:05:34 +0800

    require user to be logged in to view dashboard

--- client/src/webpages/AccountingDashboard.js
+++ client/src/webpages/AccountingDashboard.js
@@ -1,20 +1,25 @@
-import {FaMoneyBillAlt, FaHome} from 'react-icons/fa'
+import { useState } from 'react'
+import { FaMoneyBillAlt, FaHome } from 'react-icons/fa'
 import SideBar from '../components/SideBar/SideBar'
-import {Flex} from '@chakra-ui/react'
+import { Flex } from '@chakra-ui/react'
 import { Route, Routes } from 'react-router-dom'
 import Transaction from '../domain/Transaction/Transaction'
+import { Login } from '../components/LoginButton'
 
 
 export default function AccountingDashboard() {
+  const [token, setToken] = useState()
+  if (!token)
+    return <Login setToken={setToken} />
 
-  const links = [{ to:'/accounting', icon:FaHome, val:'Dashboard' }, { to:'transaction', icon:FaMoneyBillAlt, val:'Transaction' }]
+  const links = [{ to: '/accounting', icon: FaHome, val: 'Dashboard' }, { to: 'transaction', icon: FaMoneyBillAlt, val: 'Transaction' }]
 
   return (
     <Flex>
-      <SideBar links={links}/>
+      <SideBar links={links} />
       <Routes>
-        <Route path='/' element={<Home/>} />
-        <Route path="transaction" element={<Transaction/>} />
+        <Route path='/' element={<Home />} />
+        <Route path="transaction" element={<Transaction />} />
       </Routes>
     </Flex>
   )
@@ -22,7 +27,7 @@ export default function AccountingDashboard() {
 
 
 function Home() {
-  return(
+  return (
     <Flex> ok </Flex>
   )
 }
