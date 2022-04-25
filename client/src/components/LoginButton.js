1ec5088f093f468027370867a9128ba97ea69d2b
Jethro Modequillo <jethro.modequillo@mensaphilippines.org>
2022-04-25 08:18:21 +0800

    add hide/show password

--- client/src/components/LoginButton.js
+++ client/src/components/LoginButton.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import { React, useState } from 'react'
 import {
     Modal,
     ModalOverlay,
@@ -11,12 +11,14 @@ import {
     FormControl,
     FormLabel,
     Input,
+    InputGroup,
+    InputRightElement,
 } from '@chakra-ui/react'
 
 export function Login() {
     const { isOpen, onOpen, onClose } = useDisclosure()
+    const [showPassword, setShowPassword] = useState(false)
     return <>
-
         <Button bg='orange.400' onClick={onOpen}>Log in</Button>
 
         {/* Login modal */}
@@ -30,11 +32,18 @@ export function Login() {
                         <FormLabel htmlFor='email'>Email address</FormLabel>
                         <Input id='email' type='email' />
                         <FormLabel htmlFor='password'>Password</FormLabel>
-                        <Input id='password' type='password' />
+                        <InputGroup>
+
+                            <Input id='password' type={showPassword ? 'text' : 'password'} />
+                            <InputRightElement w='20%'><Button onClick={() => setShowPassword(!showPassword)} bg='none'>
+                                {showPassword ? 'Hide' : 'Show'}
+                            </Button></InputRightElement>
+                        </InputGroup>
                     </FormControl>
                 </ModalBody>
 
                 <ModalFooter>
+                    {/* TODO: This 'orange.400' is a default color from Chakra. The "correct" color for our palette is #FF7A00 but we haven't gotten around to setting up styling yet so this'll do for now */}
                     <Button mr={3} onClick={onClose} bg='orange.400' variant='with-shadow'>
                         Log In
                     </Button>
