f9c98c83d7ec1b743802e8480787b1ba80c3b997
Carryl21 <cjdelatorre1@up.edu.ph>
2022-05-21 12:46:50 +0800

    added images

--- client/src/webpages/CoursesPage.js
+++ client/src/webpages/CoursesPage.js
@@ -1,20 +1,10 @@
 import React from 'react'
-import CoursesTabsBar from '../components/CoursesTabsBar/CoursesTabsBar'
+
 
 export default function CoursesPage() {
-  const container = {
-    backgroundColor: "#FFCA84",
-    height: '82vh'
-  }
-
-  const content = {
-    paddingTop: '5em'
-  }
   return (
-    <div style={container}>
-      <div  style={content}>
-       <CoursesTabsBar/>
-      </div>
+    <div style={{backgroundColor: '#FFCA84', paddingTop: '5em'}}>
+      
     </div>
   )
 }
