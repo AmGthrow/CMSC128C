332f0b1a2e1df20f57ef63f4230c70a51e4933a7
CG Aquingel <ccpalabrica1@up.edu.ph>
2022-05-29 00:03:08 +0800

    About Us (Top-Half) - Desktop and Mobile Versions

--- client/src/domain/MainFeatures/MainFeatures.js
+++ client/src/domain/MainFeatures/MainFeatures.js
@@ -1,26 +1,27 @@
 import React from 'react'
 import MFCSS from './MainFeatures.module.css'
-import kids1 from '../../assets/kids1.jpg'
-import kids2 from '../../assets/kids2.jpg'
-import kids3 from '../../assets/kids3.jpg'
+import icon1 from '../../assets/nodes.png'
+import icon2 from '../../assets/check.png'
+import icon3 from '../../assets/wifi.png'
+import {GrFormPrevious, GrFormNext} from 'react-icons/gr'
 
 const featuresData = [{
     title: "Feature 1",
     desc: "Our learning experiences are aligned to concepts in the K12 CS Framework, Next Generation Science Standards and Singapore MOE Science Syllabus.",
     color: "#0191CB",
-    image: kids3
+    image: icon1
 },
 {
     title: "Feature 2",
     desc: "We help kids develop the holistic skills, attitudes & mindsets from the OECD Learning Compass 2030.",
     color: "#EAD842",
-    image: kids2
+    image: icon2
 },
 {
     title: "Feature 3",
     desc: "We create awareness of & inspire action towards the UN Sustainable Development Goals.",
     color: "#E55454",
-    image: kids1
+    image: icon3
 }]
 
 function FeaturesItem({featureData}) {
@@ -29,7 +30,7 @@ function FeaturesItem({featureData}) {
 
     return (
         <div className={MFCSS.feature_container} style={{backgroundColor: color }}>
-            <img src={image} alt='kids images' />
+            <img src={image} alt='icon' />
             <div className={MFCSS.description}>
                 <h1><b> {title} </b></h1>
                 <p>{desc}</p>
@@ -48,5 +49,4 @@ export default function MainFeatures () {
         </div>
     )
 }
-
 
--- client/src/webpages/AboutUsPage.js
+++ client/src/webpages/AboutUsPage.js
@@ -1,10 +1,17 @@
 import React from 'react'
-
+import MainFeatures from '../domain/MainFeatures/MainFeatures'
+import Overview from '../domain/Overview/Overview'
 
 export default function AboutUsPage() {
   return (
-    <div style={{backgroundColor: '#FFCA84', paddingTop: '5.3em'}}>
-      {/* insert code here */}
+    /* <div style={{backgroundColor: '#FFCA84', paddingTop: '5em'}}> */
+    <div>
+      <div style={{backgroundColor: '#FFCA84', paddingTop: '5em'}}>
+        <Overview/>
+      </div>
+      <div style={{paddingTop: '5em'}}>
+        <MainFeatures/>
+      </div>
     </div>
   )
 }
