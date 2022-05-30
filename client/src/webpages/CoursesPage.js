73a2d3542a59f621464beb9346421b8b8da325fe
John <91443112+KKraliSS@users.noreply.github.com>
2022-05-30 13:23:15 +0800

    Add files via upload

--- client/src/webpages/CoursesPage.js
+++ client/src/webpages/CoursesPage.js
@@ -1,21 +1,12 @@
 import React from 'react'
-import python from '../assets/python.png'
-import world from '../assets/world.png'
-import science from '../assets/science.png'
 import CoursesTabsData from '../components/CoursesTabsData/CoursesTabsData'
-
+import EmailInputField from '../domain/EmailInputField/EmailInputField'
 
 export default function CoursesPage() {
-
-  const courses = [
-    {icon: python, val:'Python in the Real World'}, 
-    {icon: world, val:'Code Meets World' },
-    {icon: science, val:'Coding With Science' }
-  ]
-
   return (
     <div style={{backgroundColor: '#FFCA84', paddingTop: '6.5em'}}>
-      <CoursesTabsData courses={courses}/>
+      <CoursesTabsData/>
+      <EmailInputField/>
     </div>
   )
 }
